e0bb03e6c685364e979bea243364aa62eeed294b
Shannon Hawn <shannonhawn@hotmail.com>
2016-04-01 16:53:41 -0400

    amended spelling of button - didn't match var, added functions

--- js/main.js
+++ js/main.js
@@ -1,20 +1,32 @@
 var $Body = $('body');
 
-var $BtnShowHide = $('.btn-show-hide');
+var $btnShowHide = $('.btn-show-hide');
 var $Box = $('.box');
 
-var $ButtonMove = $('.btn-move');
+var $btnMove = $('.btn-move');
 var $Diamond = $('.diamond');
 
-var $ButtonColEx = $('.btn-collapse-expand');
+var $btnColEx = $('.btn-collapse-expand');
 var $Panel = $('.panel');
 
-var $ButtonAppend = $('.button-append');
-var $List = $('list');
+var $btnAppend = $('.btn-append');
+var $List = $('.list');
 
-var $ButtonBounce = $('.btn-bounce');
+var $btnBounce = $('.btn-bounce');
 var $Circle = $('.circle');
 
-$btnShowHide.on ('click, function' (){
-  $box.toggleclass ('showbox');
+$btnShowHide.on('click, function' (){
+  $box.toggleClass('showbox');
+});
+
+$btnMove.on('click, function' (){
+  $diamond.toggleClass('button-slide');
+});
+
+$btnColEx.on('click, function' (){
+  $panel.toggleClass('collapse-panel');
+});
+
+$btnBounce.on('click, function' (){
+  $circle.toggleClass('bounce');
 });
