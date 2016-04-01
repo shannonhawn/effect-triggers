8722f5507d6b98662538d619dfc656c43bc7c760
Shannon Hawn <shannonhawn@hotmail.com>
2016-04-01 16:31:30 -0400

    added space

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var $Body = $('body');
+
+var $Box = $('.box');
+var $Showbox = $('.showbox');
+var $Diamond = $('.diamond');
+var $Circle = $('.circle');
+var $BtnShowHide = $('.btn-show-hide');
+var $BtnSlide = $('.btn-slide');
+var $Bounce = $('.bounce');
